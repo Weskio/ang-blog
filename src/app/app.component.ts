9237058d51855e16411681fe558b7991c97a4aa6
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2023-12-01 13:02:21 -0800

    SignUp template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SignUpComponent } from "./sign-up/sign-up.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, SignUpComponent]
 })
 export class AppComponent {
   title = 'ang-blog';
