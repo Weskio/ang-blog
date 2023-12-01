205cd8a2f1976378fb2cd1fbd2ae82147edad32d
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2023-12-01 14:26:36 -0800

    SignUp template

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    phone: new FormControl('', Validators.required)
+  })
+
+  onSubmit(){
+    alert(
+      this.profileForm.value.name + '  |  ' +  this.profileForm.value.email + '  |  ' +  this.profileForm.value.phone
+    );
+  }
+
 
 }
