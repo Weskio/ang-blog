49a1ddb4cce49485883306bdaf00e5eba3d7b57e
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2023-12-02 10:39:55 -0800

    home mobile template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SignUpComponent } from "./sign-up/sign-up.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, SignUpComponent]
+    imports: [CommonModule, RouterOutlet, SignUpComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'ang-blog';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts,scss,css}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
