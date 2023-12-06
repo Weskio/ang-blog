31f60833a5cbe3f26cdf7d57f6a89c1e876d57ce
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2023-12-06 14:03:49 -0800

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { SignUpComponent } from "./sign-up/sign-up.component";
 import { HomeComponent } from "./home/home.component";
 import {RouterModule} from '@angular/router';
+import { NewsletterComponent } from "./newsletter/newsletter.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, SignUpComponent, HomeComponent]
-    
+    imports: [CommonModule, RouterOutlet, SignUpComponent, HomeComponent, NewsletterComponent]
 })
 export class AppComponent {
   title = 'ang-blog';
